7a68ccc41b19f667cd09c4405252d3fc101650be
Khurram Virani <kvirani@gmail.com>
2015-05-19 10:27:37 -0400

    refactored connection into its own public class method

--- kitten.rb
+++ kitten.rb
@@ -12,12 +12,7 @@ class Kitten
 
   class << self 
     def all
-      conn = PG::Connection.new(
-        host: 'ec2-54-197-241-97.compute-1.amazonaws.com',
-        password: '5nwKtDvZcm2XOf2A__s4J8q_EN',
-        user: 'tlmebsdzoqdlis',
-        dbname: 'd5ns2am3lp4r0g'
-      )
+      conn = establish_connection
 
       results = conn.exec( "SELECT * FROM kittens;" )
 
@@ -32,19 +27,26 @@ class Kitten
 
       kittens
     end
+
+    def establish_connection
+      conn = PG::Connection.new(
+        host: 'ec2-54-197-241-97.compute-1.amazonaws.com',
+        password: '5nwKtDvZcm2XOf2A__s4J8q_EN',
+        user: 'tlmebsdzoqdlis',
+        dbname: 'd5ns2am3lp4r0g'
+      )
+    end
+
   end
 
 
   def save
-    conn = PG::Connection.new(
-      host: 'ec2-54-197-241-97.compute-1.amazonaws.com',
-      password: '5nwKtDvZcm2XOf2A__s4J8q_EN',
-      user: 'tlmebsdzoqdlis',
-      dbname: 'd5ns2am3lp4r0g'
-    )
-    sql = "UPDATE kittens SET name = '#{name}', laser_strength = #{laser_strength}, updated_at = '#{Time.now}' WHERE id = #{id}"
+    conn = Kitten.establish_connection
+    sql = "UPDATE kittens SET name = '#{name}', laser_strength = #{laser_strength}, updated_at = '#{Time.now}' WHERE id = #{id};"
     result = conn.exec(sql)
     result.result_status == PG::PGRES_COMMAND_OK
   end
+
+  
 
 end
--- main.rb
+++ main.rb
@@ -1,24 +1,25 @@
 require 'pg'
 require_relative 'kitten'
 
-kittens = Kitten.all? { |e|  }
+kittens = Kitten.all
 puts kittens.inspect
 
 k = kittens.first
-k.name = 'Ralfy' # make change in memory
+k.name = 'Charlie' # make change in memory
 puts k.save.inspect # this doesn't exist yet, so we need to implement it!
 
+puts Kitten.all.inspect
 
 # Kitten API SPEC
 
-Kitten.all # SELECT => []
-kitten.save # UPDATE
-k = Kitten.find_by_id(id)
-k.destroy # => DELETE 
+# Kitten.all # SELECT => []
+# kitten.save # UPDATE
+# k = Kitten.find_by_id(id)
+# k.destroy # => DELETE 
 
-k2 = Kitten.new(...) # What about ID here? DB sets this
-k2.name = 'fdsafsa'
-k2.save # => INSERT 
+# k2 = Kitten.new(...) # What about ID here? DB sets this
+# k2.name = 'fdsafsa'
+# k2.save # => INSERT 
 
 
 
